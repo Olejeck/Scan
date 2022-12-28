25c15226ad4184ee693572aaffb23bb15cf23e24
Olejeck <oleg.navrotskiy082007@gmail.com>
2022-12-28 19:15:12 +0300

    Scanning text from terminal

---
+++ src/Scan.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.*;
+import java.util.*;
+
+public class Scan {
+    public static void main(String[] args) throws Exception{
+        Scanner s = new Scanner(System.in);
+        ArrayList<String> arrayList = new ArrayList<>();
+        while (true) {
+            String sc = s.nextLine();
+            arrayList.add(sc + " ");
+            if (sc.endsWith("exit")) {
+                break;
+            }
+            System.out.println("хочу ещё!!!");
+        }
+        System.out.println(arrayList);
+    }
+}
